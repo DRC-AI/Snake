f7ed67a3b70d655010e105aecb0857fefda75e4c
DRC-AI <65873672+DRC-AI@users.noreply.github.com>
2022-05-29 16:36:30 +0200

    Add files via upload

---
+++ scoretracker.py
@@ -0,0 +1,44 @@
+import random
+class scoreTracker():
+    def __init__(self):
+        self.positionx = 0
+        self.positiony = 0
+        self.score = 0
+    
+    def isPoint(self, snakePosition):
+        '''checks if the snake head overlaps with point position'''
+        
+        pointPosition = self.getPosition()
+
+        if pointPosition == snakePosition:
+            return True
+        else :
+            return False
+
+    def increaseScore(self):
+        '''Increases score by 1'''
+
+        self.score = self.score + 100
+
+
+
+    def getPosition(self):
+        '''This functions returns position in a tuple'''
+
+        position = [ self.positionx , self.positiony ]
+
+        return position
+
+    def generatePoint(self, window):
+        '''Generates a point with random position'''
+
+        y, x = window.getmaxyx()
+       
+        self.positionx = random.randint(1, x -2)
+        self.positiony = random.randint(1, y -2)
+
+    def render(self, window):
+        '''Renders point in passed window'''
+
+        window.addstr(self.positiony, self.positionx, "*")
+    
